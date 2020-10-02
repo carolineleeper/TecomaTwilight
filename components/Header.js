299669788006cd8aa9443f335d8d4ed17c22d0a1
Caroline Leeper <caroline.leeper@gmail.com>
2020-10-03 00:19:22 +1000

    UPDATE: add placeholder styles

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,12 @@
 import Head from "next/head";
 
 const Header = () => {
-  return <h1>Tecoma Twilight</h1>;
+  return (
+    <>
+      <h1>Tecoma Twilight</h1>
+      <h2>ETHICAL MARKET</h2>
+    </>
+  );
 };
 
 export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import SEO from "../components/SEO";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
+import "../style.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
