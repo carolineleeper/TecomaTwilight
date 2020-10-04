b38ac397858d15bf85ec5302512b6ab1871daeb4
Caroline Leeper <caroline.leeper@gmail.com>
2020-10-04 22:02:18 +1100

    UPDATE: stall card details

--- pages/stalls/[stall].js
+++ pages/stalls/[stall].js
@@ -7,7 +7,11 @@ const Stall = (props) => {
     <>
       <h2>{props.data.name}</h2>
       <p>Website: {props.data.url}</p>
+      <p>Criteria: {props.data.criteria}</p>
+      <p>Categories: {props.data.category}</p>
       <div dangerouslySetInnerHTML={{ __html: props.html }} />
+      <h2>Products:</h2>
+      <p>{props.data.products}</p>
     </>
   );
 };
