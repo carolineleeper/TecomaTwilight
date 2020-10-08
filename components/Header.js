f649e0154a887568d88860e9e7c1bac1a1c5d657
Caroline Leeper <caroline.leeper@gmail.com>
2020-10-08 15:29:19 +1100

    UPDATE: add header scroll animation

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,8 @@
 import Logo from "./Logo";
 import Nav from "./Nav";
 import style from "./Header.module.css";
-import { useEffect, useState } from "react";
 
 const Header = () => {
-  // const [shouldCollapse, setShouldCollapse] = useState(false);
-
-  // useEffect(() => {
-  //   window.onscroll = () => {
-  //     if (
-  //       document.body.scrollTop > 80 ||
-  //       document.documentElement.scrollTop > 80
-  //     ) {
-  //       setShouldCollapse(true);
-  //     } else {
-  //       setShouldCollapse(false);
-  //     }
-  //   };
-  // }, []);
-
   return (
     <>
       <div className={style.headerContainer}>
