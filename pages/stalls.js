d163ca00a9f40cf1a1e5cd649c256a0e2abdfcd8
Caroline Leeper <caroline.leeper@gmail.com>
2020-10-12 20:27:23 +1100

    UPDATE: shuffle display of stalls

--- pages/stalls.js
+++ pages/stalls.js
@@ -60,7 +60,27 @@ const Stalls = (props) => {
       return intersectChecker(mergedArray, checkArray);
     });
 
-    return filtered;
+    const shuffleArray = (array) => {
+      let currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    };
+
+    return shuffleArray(filtered);
   };
 
   console.log(`Numner: ${filteredStalls().length}`);
