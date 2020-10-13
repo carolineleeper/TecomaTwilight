f370019ad5422bd3d008e6762ae92da5d203ada9
Caroline Leeper <caroline.leeper@gmail.com>
2020-10-13 21:30:49 +1100

    RESET: coming soon page

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,7 @@ const Home = () => {
   return (
     <>
       <SEO title="Home" />
-      <h1>Home</h1>
-      <p>
-        The perfect pre-Christmas market – promoting living and buying
-        ethically.
-      </p>
-      <p>
-        This twilight market supports ethical trading, community empowerment and
-        sustainable practices. Spending your money at any of the 60 plus stalls
-        will help create positive change locally and globally.
-      </p>
-      <p>
-        With quality, local food vendors and a stellar line up of local
-        performers, this festival-like market will be a great community event
-        for the whole family.
-      </p>
+      <h1>Coming Soon</h1>
     </>
   );
 };
