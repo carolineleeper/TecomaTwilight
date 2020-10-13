0dca5b1a78db06c844aeeec7418b05210b717fdb
Caroline Leeper <caroline.leeper@gmail.com>
2020-10-14 00:12:15 +1100

    add stallholder confirmation form

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,5 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import "../style.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <div className="pageContent">
-        <Component {...pageProps} />
-      </div>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
---
+++ pages/stallholder-confirmation.js
@@ -0,0 +1,9 @@
+const Confirmation = () => {
+  return (
+    <div className="formsStudio">
+      <iframe src="https://script.google.com/macros/s/AKfycbwwDr3sffQNigc0ahB0S94l8xRAjf7w5t6SFvh5pGeCSB-psFA/exec"></iframe>
+    </div>
+  );
+};
+
+export default Confirmation;
