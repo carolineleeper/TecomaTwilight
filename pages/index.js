7bd040d0ecb0c66467f040f79dd121ebd574deea
Caroline Leeper <caroline.leeper@gmail.com>
2020-10-16 13:55:37 +1100

    update coming soon text

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,17 @@ const Home = () => {
       <SEO title="Coming Soon" />
       <div className="comingSoonContainer">
         <Logo />
-        <h1 className="comingSoon">Coming Soon</h1>
+        <h1 className="comingSoon">Launching November 15</h1>
         <div className="applyHereContainer">
-        <p className="applyHere">Are you an ethical business wanting to feature in this market?</p>
-        <a className="applyBtn" href="https://docs.google.com/forms/d/e/1FAIpQLSdMb5r3Vr8q8jMA_9mIYrJriC1u3vZT2dRFi9JfTU1A-B6I2w/viewform?usp=sf_link"><button>Apply Here</button></a>
+          <p className="applyHere">
+            Are you an ethical business wanting to feature in this market?
+          </p>
+          <a
+            className="applyBtn"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSdMb5r3Vr8q8jMA_9mIYrJriC1u3vZT2dRFi9JfTU1A-B6I2w/viewform?usp=sf_link"
+          >
+            <button>Apply Here</button>
+          </a>
         </div>
       </div>
     </>
