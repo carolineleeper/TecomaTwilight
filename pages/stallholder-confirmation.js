fe222254173988cd5cc4ff507493782c7a2ac4d7
Caroline Leeper <caroline.leeper@gmail.com>
2020-10-17 15:01:30 +1100

    add og meta tags

--- pages/stallholder-confirmation.js
+++ pages/stallholder-confirmation.js
@@ -1,7 +1,7 @@
 const Confirmation = () => {
   return (
     <>
-      <SEO title="Stallholder Confirmation" />
+      {/* <SEO title="Stallholder Confirmation" /> */}
       <div className="formsStudio">
         <iframe src="https://script.google.com/macros/s/AKfycbwwDr3sffQNigc0ahB0S94l8xRAjf7w5t6SFvh5pGeCSB-psFA/exec"></iframe>
       </div>
