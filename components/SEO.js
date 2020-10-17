d9117d7a28fc0d2dd7d0eeed39b24e11df27bf21
Caroline Leeper <caroline.leeper@gmail.com>
2020-10-17 15:15:22 +1100

    add og:image:secure_url meta tag

--- components/SEO.js
+++ components/SEO.js
@@ -37,6 +37,12 @@ const SEO = (props) => {
         // href="https://tecomatwilight.com/static/og_image.png"
         key="ogimage"
       />
+      <meta
+        property="og:image:secure_url"
+        content="tecomatwilight.com/static/og_image.png"
+        // href="https://tecomatwilight.com/static/og_image.png"
+        key="ogimage"
+      />
 
       {/* <!-- Twitter --> */}
       <meta property="twitter:card" content="summary_large_image" />
@@ -48,6 +54,11 @@ const SEO = (props) => {
       <meta
         property="twitter:description"
         content="This market supports ethical trading, community empowerment and sustainable practices. Spending your money at any of the stalls will help create positive change locally and globally."
+      />
+      <meta
+        property="twitter:image"
+        content="tecomatwilight.com/static/og_image.png"
+        // href="https://tecomatwilight.com/static/og_image.png"
       />
       <meta
         property="twitter:image"
