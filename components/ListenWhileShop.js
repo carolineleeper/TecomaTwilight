bde77c2aa091db45b0bbe2f11b6bd35157a1901e
Caroline Leeper <caroline.leeper@gmail.com>
2020-10-19 21:31:31 +1100

    add hide button for spotify player

--- components/ListenWhileShop.js
+++ components/ListenWhileShop.js
@@ -3,7 +3,7 @@ import style from "./ListenWhileShop.module.css";
 const ListenWhileShop = () => {
   return (
     <div className={style.listen}>
-      <p>Listen while you shop</p>
+      <div>Listen while you shop</div>
     </div>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import ListenWhileShop from "../components/ListenWhileShop";
+import SpotifyContainer from "../components/SpotifyContainer";
 import "../style.css";
 import Head from "next/head";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import SpotifyPlayer from "react-spotify-player";
 
-export default function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
@@ -19,17 +18,11 @@ export default function MyApp({ Component, pageProps }) {
       <Header />
       <div className="pageContent">
         <Component {...pageProps} />
-        <div className="spotify">
-          <ListenWhileShop />
-          <SpotifyPlayer
-            uri="spotify:album:1TIUsv8qmYLpBEhvmBmyBk"
-            size="compact"
-            view="list"
-            theme="white"
-          />
-        </div>
+        <SpotifyContainer />
       </div>
       <Footer />
     </>
   );
-}
+};
+
+export default MyApp;
