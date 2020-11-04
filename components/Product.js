7ae071f3b859219e610e313cf554c4ae02cdc899
Caroline Leeper <caroline.leeper@gmail.com>
2020-11-04 21:50:23 +1100

    update product display

--- components/Product.js
+++ components/Product.js
@@ -5,14 +5,16 @@ const Product = (props) => {
     <div className={style.productCard}>
       {/* <p>Price: {props.product_price}</p> */}
       <div className={style.imageContainer}>
-        <img
-          src={props.product_image}
-          alt={`picture of ${props.product_name}`}
-        ></img>
+        {props.product_image && (
+          <img
+            src={props.product_image}
+            // alt={`picture of ${props.product_name}`}
+          ></img>
+        )}
       </div>
       <div className={style.productInfo}>
-        <h2>{props.product_name}</h2>
-        <p>{props.product_description}</p>
+        {props.product_name && <h2>{props.product_name}</h2>}
+        {props.product_description && <p>{props.product_description}</p>}
       </div>
     </div>
   );
