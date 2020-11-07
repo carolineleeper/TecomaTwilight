fc2fbe84f15b6d609f310dd4a305c9e2d9d5cf31
Caroline Leeper <caroline.leeper@gmail.com>
2020-11-07 16:50:23 +1100

    add eat page stalls

--- components/EatPage.js
+++ components/EatPage.js
@@ -1,8 +1,35 @@
-const EatPage = () => {
+import StallCard from "./StallCard";
+import Link from "next/link";
+
+const featuredFoodVendorNames = [
+  "The Eatery Tecoma",
+  "Proserpina Bakehouse",
+  "Babaji's Kitchen",
+  "The Dolly Bus",
+  "Nevedya",
+  "Blacksmith",
+];
+
+const EatPage = (props) => {
+  const foodVendors = props.stalls.filter((stall) => {
+    console.log(stall.name);
+    return featuredFoodVendorNames.includes(stall.name);
+  });
+
   return (
     <>
       <h1 className="title">EAT</h1>
-      <p>List of food vendors TBC</p>
+      <ul className="galleryContainer">
+        {foodVendors.map((stall) => {
+          return (
+            <Link key={stall.filename} href={`stalls/${stall.filename}`}>
+              <a className="stallLink">
+                <StallCard stall={stall} />
+              </a>
+            </Link>
+          );
+        })}
+      </ul>
     </>
   );
 };
--- pages/eat.js
+++ pages/eat.js
@@ -1,13 +1,30 @@
 import SEO from "../components/SEO";
 import EatPage from "../components/EatPage";
+import matter from "gray-matter";
+import fs from "fs";
 
-const Eat = () => {
+const Eat = (props) => {
   return (
     <>
       <SEO title="Eat" />
-      <EatPage />
+      <EatPage stalls={props.stalls} />
     </>
   );
+};
+
+export const getStaticProps = () => {
+  const directory = `${process.cwd()}/stalls`;
+  const rawFilenames = fs.readdirSync(directory);
+
+  const stalls = rawFilenames.map((filename) => {
+    const rawFileContent = fs
+      .readFileSync(`${directory}/${filename}`)
+      .toString();
+    const { data } = matter(rawFileContent);
+    return { ...data, filename: filename.replace(".md", "") };
+  });
+
+  return { props: { stalls } };
 };
 
 export default Eat;
