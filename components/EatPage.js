16f0f1c51ff91c9ab2a40d35a45090142782c9d8
Caroline Leeper <caroline.leeper@gmail.com>
2020-11-07 17:32:58 +1100

    filter food vendor stalls on eat.js prior to passing to EatPage component

--- components/EatPage.js
+++ components/EatPage.js
@@ -1,26 +1,12 @@
 import StallCard from "./StallCard";
 import Link from "next/link";
 
-const featuredFoodVendorNames = [
-  "The Eatery Tecoma",
-  "Proserpina Bakehouse",
-  "Babaji's Kitchen",
-  "The Dolly Bus",
-  "Nevedya",
-  "Blacksmith",
-];
-
 const EatPage = (props) => {
-  const foodVendors = props.stalls.filter((stall) => {
-    console.log(stall.name);
-    return featuredFoodVendorNames.includes(stall.name);
-  });
-
   return (
     <>
       <h1 className="title">EAT</h1>
       <ul className="galleryContainer">
-        {foodVendors.map((stall) => {
+        {props.stalls.map((stall) => {
           return (
             <Link key={stall.filename} href={`stalls/${stall.filename}`}>
               <a className="stallLink">
--- pages/eat.js
+++ pages/eat.js
@@ -3,6 +3,15 @@ import EatPage from "../components/EatPage";
 import matter from "gray-matter";
 import fs from "fs";
 
+const featuredFoodVendorNames = [
+  "The Eatery Tecoma",
+  "Proserpina Bakehouse",
+  "Babaji's Kitchen",
+  "The Dolly Bus",
+  "Nevedya",
+  "Blacksmith",
+];
+
 const Eat = (props) => {
   return (
     <>
@@ -16,13 +25,17 @@ export const getStaticProps = () => {
   const directory = `${process.cwd()}/stalls`;
   const rawFilenames = fs.readdirSync(directory);
 
-  const stalls = rawFilenames.map((filename) => {
-    const rawFileContent = fs
-      .readFileSync(`${directory}/${filename}`)
-      .toString();
-    const { data } = matter(rawFileContent);
-    return { ...data, filename: filename.replace(".md", "") };
-  });
+  const stalls = rawFilenames
+    .map((filename) => {
+      const rawFileContent = fs
+        .readFileSync(`${directory}/${filename}`)
+        .toString();
+      const { data } = matter(rawFileContent);
+      return { ...data, filename: filename.replace(".md", "") };
+    })
+    .filter((stall) => {
+      return featuredFoodVendorNames.includes(stall.name);
+    });
 
   return { props: { stalls } };
 };
