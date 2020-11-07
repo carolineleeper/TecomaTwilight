ccfa65ea7b23691bf6d082a1b28fcf242e0de70d
Caroline Leeper <caroline.leeper@gmail.com>
2020-11-07 15:41:05 +1100

    add display image only for products with no title

--- components/Product.js
+++ components/Product.js
@@ -2,21 +2,34 @@ import style from "./Product.module.css";
 
 const Product = (props) => {
   return (
-    <div className={style.productCard}>
-      {/* <p>Price: {props.product_price}</p> */}
-      <div className={style.imageContainer}>
-        {props.product_image && (
-          <img
-            src={props.product_image}
-            // alt={`picture of ${props.product_name}`}
-          ></img>
-        )}
-      </div>
-      <div className={style.productInfo}>
-        {props.product_name && <h2>{props.product_name}</h2>}
-        {props.product_description && <p>{props.product_description}</p>}
-      </div>
-    </div>
+    <>
+      {props.product_name ? (
+        <div className={style.productCard}>
+          {/* <p>Price: {props.product_price}</p> */}
+          <div className={style.imageContainer}>
+            {props.product_image && (
+              <img
+                src={props.product_image}
+                // alt={`picture of ${props.product_name}`}
+              ></img>
+            )}
+          </div>
+          <div className={style.productInfo}>
+            {props.product_name && <h2>{props.product_name}</h2>}
+            {props.product_description && <p>{props.product_description}</p>}
+          </div>
+        </div>
+      ) : (
+        props.product_image && (
+          <div className={style.imageOnlyContainer}>
+            <img
+              src={props.product_image}
+              // alt={`picture of ${props.product_name}`}
+            ></img>
+          </div>
+        )
+      )}
+    </>
   );
 };
 
