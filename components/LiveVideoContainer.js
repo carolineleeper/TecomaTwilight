c4663efe88a671e8fddb45c02db2a46c90060914
Caroline Leeper <caroline.leeper@gmail.com>
2020-11-09 23:37:39 +1100

    add twitch embed

--- components/LiveVideoContainer.js
+++ components/LiveVideoContainer.js
@@ -1,31 +1,34 @@
 import style from "./LiveVideoContainer.module.css";
 import ReactPlayer from "react-player";
+import { TwitchPlayer } from "react-twitch-embed";
 
 const LiveVideoContainer = () => {
   return (
     <div className={style.liveVideoContainer}>
       <h1 className="title">Watch Live</h1>
       <div className={style.responsiveVideoContainer}>
+        <TwitchPlayer
+          channel="tecomatwilight"
+          id="tecomatwilight"
+          theme="light"
+          muted
+          width="100%"
+          // withChat={false}
+          onVideoPause={() => console.log(":(")}
+        />
+
         {/* <ReactPlayer
           className={style.reactPlayer}
           url="https://youtu.be/sDqaRiUJ_qo"
           width="100%"
           height="100%"
         /> */}
-        <ReactPlayer
+        {/* <ReactPlayer
           className={style.reactPlayer}
           url="https://player.twitch.tv/?channel=carothedev&parent=netlify.app"
           width="100%"
           height="100%"
-        />
-
-        <iframe
-          src="https://player.twitch.tv/?channel=carothedev&parent=netlify.app"
-          width="100%"
-          height="315"
-          frameBorder="0"
-          allowFullScreen
-        ></iframe>
+        />*/}
       </div>
     </div>
   );
