bbf62b52534937f24d8f0bc1a99106db46f8a562
Caroline Leeper <caroline.leeper@gmail.com>
2020-11-11 21:05:30 +1100

    update hemslojdjoy images

--- components/VideoPlaylistContainer.js
+++ components/VideoPlaylistContainer.js
@@ -5,10 +5,10 @@ const VideoPlaylistContainer = () => {
   return (
     <div className={style.videoPlaylistContainer}>
       <div className={style.videoPlaylistContainerText}>
-        <h1 className="title"></h1>
+        <h1 className="title">Get Ethical!</h1>
         <p>
-          Our stall holders talk about cool stuff. We discuss ethical approaches
-          and other things.
+          Check out the videos below to hear our stall holders talk about all
+          things ethical
         </p>
       </div>
       <Video src="https://www.youtube.com/embed/videoseries?list=PL735C37C69C6A737C" />
