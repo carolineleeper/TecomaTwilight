07571c2a360022de916e43af27b59e68a5d26d4f
Caroline Leeper <caroline.leeper@gmail.com>
2020-11-11 21:53:27 +1100

    change twitch embed to react player

--- pages/abcdefghi.js
+++ pages/abcdefghi.js
@@ -1,17 +1,24 @@
 // import style from "./LiveVideoContainer.module.css";
+import ReactPlayer from "react-player";
 import { TwitchPlayer } from "react-twitch-embed";
 
 const Test = () => {
   return (
-    <TwitchPlayer
-      channel="tecomatwilight"
-      id="tecomatwilight"
-      theme="dark"
-      muted
+    <ReactPlayer
+      url="https://www.twitch.tv/tecomatwilight"
       width="100%"
       height="100%"
-      // autoplay={false}
+      controls={false}
     />
+    // <TwitchPlayer
+    //   channel="tecomatwilight"
+    //   id="tecomatwilight"
+    //   theme="dark"
+    //   muted
+    //   width="100%"
+    //   height="100%"
+    //   // autoplay={false}
+    // />
   );
 };
 
