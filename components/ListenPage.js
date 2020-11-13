c917e8907561c7ed33a2545afff7c528b6b03bc0
Caroline Leeper <caroline.leeper@gmail.com>
2020-11-13 21:15:03 +1100

    add style module to listen page

--- components/ListenPage.js
+++ components/ListenPage.js
@@ -1,8 +1,12 @@
+import style from "./ListenPage.module.css";
+
 const ListenPage = () => {
   return (
     <>
       <h1 className="title">Listen</h1>
-      <p>List of local artists TBC</p>
+      <div className={style.textContainer}>
+        <p className="upperCase">List of local artists TBC</p>
+      </div>
     </>
   );
 };
