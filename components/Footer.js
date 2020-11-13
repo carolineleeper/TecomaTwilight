322bd6ddec779e2164c3721a42205911411393c5
Caroline Leeper <caroline.leeper@gmail.com>
2020-11-13 22:08:18 +1100

    add contact details to footer

--- components/Footer.js
+++ components/Footer.js
@@ -9,22 +9,38 @@ import style from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={style.footer}>
-      <div className={style.socialsContainer}>
-        <Facebook
-          key="facebook"
-          href="https://www.facebook.com/TecomaTwilightEthicalMarket/"
-        />
-        <Instagram
-          key="instagram"
-          href="https://www.instagram.com/tecomatwilightmarket/"
-        />
+      <div className={style.contactsContainer}>
+        <div className={style.socialsContainer}>
+          <Facebook
+            key="facebook"
+            href="https://www.facebook.com/TecomaTwilightEthicalMarket/"
+          />
+          <Instagram
+            key="instagram"
+            href="https://www.instagram.com/tecomatwilightmarket/"
+          />
+        </div>
+        <p>
+          <i aria-hidden className="far fa-envelope"></i>{" "}
+          <a href="mailto:tecomatwilightmarket@gmail.com">
+            tecomatwilightmarket@gmail.com
+          </a>
+        </p>
+        <p>
+          <i aria-hidden className="fas fa-map-marker-alt"></i> Tecoma Uniting
+          Church, 1566 Burwood Highway, Tecoma
+        </p>
+        <p>
+          <i aria-hidden className="fas fa-phone"></i>{" "}
+          <a href="tel:0397542177">(03) 9754 2177</a>
+        </p>
       </div>
+      <Acknowledgement />
       <div className={style.copyrightContainer}>
         <Copyright />
         <p>|</p>
         <CAROtheDEV />
       </div>
-      <Acknowledgement />
     </footer>
   );
 };
