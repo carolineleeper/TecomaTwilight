3d9d52443f8fdcd8c57a5bdbb0457cf920dc20fa
Caroline Leeper <caroline.leeper@gmail.com>
2020-11-15 12:54:42 +1100

    add listen page text

--- components/ListenPage.js
+++ components/ListenPage.js
@@ -5,14 +5,24 @@ const ListenPage = () => {
     <>
       <h1 className="title">Listen</h1>
       <div className={style.textContainer}>
-        {/* {musicians.map((musician) => {
-          return (
-            <>
-              <p className={style.name}>{musician.name}</p>
-              <p>{musician.description}</p>
-            </>
-          );
-        })} */}
+        <p>
+          We are SO lucky to have a vibrant and alive music scene in the
+          Dandenong Ranges and surrounds. Such a cornucopia of talent and
+          generosity from professional artists who live and work here. So many
+          of them support local community events, often giving their time and
+          skill for little or no money while working to support families and
+          ploughing their earnings back into the local economy.
+        </p>
+        <p>
+          Sadly, many have gone from gigs galore to no gigs over this extended
+          period of Covid-19.
+        </p>
+        <p className="upperCase">
+          Tecoma Ethical Twilight Market offers you the opportunity to support
+          and thank them by listening and paying for their services. You can
+          book them for an event or buy albums for pressies for the Festival
+          Season. Follow the links and Happy shopping!
+        </p>
       </div>
     </>
   );
--- musicians/dave-mcfarlane.md
+++ musicians/dave-mcfarlane.md
@@ -1,6 +1,6 @@
 ---
 name: "Dave McFarlane"
-image: "/images/musicians/davemacfarlane.png"
+image: "/images/musicians/davemcfarlane.jpg"
 ---
 
 Dave McFarlane is an award-winning songwriter, guitarist and vocalist with five independent CD releases & regular national radio airplay. With over 25 years performance experience, he has appeared at major festivals and events throughout Australia. An accomplished acoustic & electric blues and slide guitarist who also plays harmonica, mandolin, electric & double bass (plus a little cello) Dave specializes in composing in open tunings and counts traditional music styles plus rock bands of the 70’s and 80’s as his early influences.
