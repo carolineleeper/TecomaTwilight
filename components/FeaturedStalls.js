ae36df27c6eb389951c9f12eb41c13efd78d81a9
Caroline Leeper <caroline.leeper@gmail.com>
2020-11-17 19:59:37 +1100

    set new go live date for featured stalls

--- components/FeaturedStalls.js
+++ components/FeaturedStalls.js
@@ -11,7 +11,7 @@ const getWrappedIndex = (collection, index) => {
 };
 
 const FeaturedStalls = ({ stalls }) => {
-  const goLiveDate = new Date("2020-10-29");
+  const goLiveDate = new Date("2020-11-15");
   const today = new Date();
   const diffTime = today - goLiveDate;
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
