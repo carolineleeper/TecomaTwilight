866066cd3351c108467e50efc8ca4fbcfa54044f
Caroline Leeper <caroline.leeper@gmail.com>
2021-11-14 21:19:36 +1100

    remove components from homepage

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,29 @@
 import SEO from "../components/SEO";
-import FeaturedStalls from "../components/FeaturedStalls";
-import LiveVideoContainer from "../components/LiveVideoContainer";
-import VideoPlaylistContainer from "../components/VideoPlaylistContainer";
-import Apply from "../components/Apply";
 import InlineImage from "../components/InlineImage";
 
 import matter from "gray-matter";
 import fs from "fs";
 
 const Home = (props) => {
-  return (
-    <>
-      <SEO title="Home" />
-      <InlineImage src="/images/DSC_0161.jpg" align="imageRight" />
-      <LiveVideoContainer />
-      <FeaturedStalls stalls={props.stalls} />
-      <VideoPlaylistContainer />
-      <Apply />
-    </>
-  );
+	return (
+		<>
+			<SEO title="Home" />
+			<InlineImage src="/images/DSC_0161.jpg" align="imageRight" />
+		</>
+	);
 };
 
 export const getStaticProps = () => {
-  const directory = `${process.cwd()}/stalls`;
-  const rawFilenames = fs.readdirSync(directory);
+	const directory = `${process.cwd()}/stalls`;
+	const rawFilenames = fs.readdirSync(directory);
 
-  const stalls = rawFilenames.map((filename) => {
-    const rawFileContent = fs
-      .readFileSync(`${directory}/${filename}`)
-      .toString();
-    const { data } = matter(rawFileContent);
-    return { ...data, filename: filename.replace(".md", "") };
-  });
+	const stalls = rawFilenames.map((filename) => {
+		const rawFileContent = fs.readFileSync(`${directory}/${filename}`).toString();
+		const { data } = matter(rawFileContent);
+		return { ...data, filename: filename.replace(".md", "") };
+	});
 
-  return { props: { stalls } };
+	return { props: { stalls } };
 };
 
 export default Home;
