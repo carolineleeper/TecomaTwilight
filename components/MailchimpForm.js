550931a002de3a90529a5ff62908f99fc848dfb9
Caroline Leeper <caroline.leeper@gmail.com>
2021-11-15 20:52:47 +1100

    style mailchimp form fields

--- components/MailchimpForm.js
+++ components/MailchimpForm.js
@@ -3,32 +3,34 @@ import style from "./MailchimpForm.module.css";
 
 const MailchimpForm = () => {
 	return (
-		<div>
+		<>
 			<h1 className="title">Be the first to know!</h1>
-			<Mailchimp
-				action="https://tecomatwilight.us20.list-manage.com/subscribe/post?u=b4dbb5133c7fee517e2253af7&amp;id=7cb9893fd6"
-				fields={[
-					{
-						name: "EMAIL",
-						placeholder: "Email",
-						type: "email",
-						required: true,
-					},
-					{
-						name: "FNAME",
-						placeholder: "First name",
-						type: "text",
-						required: false,
-					},
-					{
-						name: "LNAME",
-						placeholder: "Last name",
-						type: "text",
-						required: false,
-					},
-				]}
-			/>
-		</div>
+			<div className={style.formContainer}>
+				<Mailchimp
+					action="https://tecomatwilight.us20.list-manage.com/subscribe/post?u=b4dbb5133c7fee517e2253af7&amp;id=7cb9893fd6"
+					fields={[
+						{
+							name: "EMAIL",
+							placeholder: "Email",
+							type: "email",
+							required: true,
+						},
+						{
+							name: "FNAME",
+							placeholder: "First name",
+							type: "text",
+							required: false,
+						},
+						{
+							name: "LNAME",
+							placeholder: "Last name",
+							type: "text",
+							required: false,
+						},
+					]}
+				/>
+			</div>
+		</>
 	);
 };
 
