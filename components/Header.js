fee837489d15362e35afef7880781c84bc06fa4b
Caroline Leeper <caroline.leeper@gmail.com>
2022-08-29 22:19:06 +1000

    delete eat page

--- components/Header.js
+++ components/Header.js
@@ -5,41 +5,37 @@ import style from "./Header.module.css";
 import { useMediaQuery } from "react-responsive";
 
 const routes = [
-  {
-    title: "Home",
-    url: "/",
-  },
-  {
-    title: "About",
-    url: "/about",
-  },
-  {
-    title: "Browse",
-    url: "/stalls",
-  },
-  {
-    title: "Eat",
-    url: "/eat",
-  },
-  {
-    title: "Listen",
-    url: "/musicians",
-  },
+	{
+		title: "Home",
+		url: "/",
+	},
+	{
+		title: "About",
+		url: "/about",
+	},
+	{
+		title: "Browse",
+		url: "/stalls",
+	},
+	{
+		title: "Listen",
+		url: "/musicians",
+	},
 ];
 
 const Header = () => {
-  const isMobile = useMediaQuery({
-    query: "(max-width: 750px)",
-  });
+	const isMobile = useMediaQuery({
+		query: "(max-width: 750px)",
+	});
 
-  return (
-    <>
-      <div className={style.headerContainer}>
-        <Logo />
-        {isMobile ? <NavMobile routes={routes} /> : <Nav routes={routes} />}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={style.headerContainer}>
+				<Logo />
+				{isMobile ? <NavMobile routes={routes} /> : <Nav routes={routes} />}
+			</div>
+		</>
+	);
 };
 
 export default Header;
